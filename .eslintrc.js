907397413603cf318f33e86f9f5bda26d70e3b29
JoMaRoNe <jmromero@duck.com>
2024-04-08 20:16:44 +0200

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
     "env": {
-        "node": true,
+        "browser": true,
         "commonjs": true,
         "es2021": true
     },
@@ -37,6 +37,14 @@ module.exports = {
         "semi": [
             "error",
             "never"
+        ],
+        'eqeqeq': 'error',
+        'no-trailing-spaces': 'error',
+        'object-curly-spacing': [
+            'error', 'always'
+        ],
+        'arrow-spacing': [
+            'error', { 'before': true, 'after': true }
         ]
     },
     "ignores": {
