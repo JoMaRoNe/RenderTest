99e9b7d51bd43f83e2df61225992e2a22c07e509
JoMaRoNe <jmromero@duck.com>
2024-04-09 18:55:17 +0200

    lint applied 	modified:   .eslintrc.js 	new file:   eslint.config.mjs 	modified:   index.js 	modified:   models/note.js 	modified:   package-lock.json 	modified:   package.json 	modified:   utils/config.js 	modified:   utils/logger.js

--- models/note.js
+++ models/note.js
@@ -7,11 +7,11 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url)
-  .then(result => {    
-    console.log('connected to MongoDB')  
-  })  
-  .catch((error) => {    
-    console.log('error connecting to MongoDB:', error.message)  
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
   })
 
 const noteSchema = new mongoose.Schema({
@@ -20,7 +20,7 @@ const noteSchema = new mongoose.Schema({
     minLength: 5,
     required: true
   },
-  important: Boolean,
+  important: Boolean
 })
 
 noteSchema.set('toJSON', {
